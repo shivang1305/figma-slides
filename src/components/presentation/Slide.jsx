351334cd617473a2f894581f2a09b00b81d9b26f
Shivang Sharma <sharmashivang1999@gmail.com>
2024-01-26 23:16:43 +0530

    feat: added bounds to the draggable container

--- src/components/presentation/Slide.jsx
+++ src/components/presentation/Slide.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useRef } from "react";
 import { animated, useSpring } from "react-spring";
 import { useDrag } from "@use-gesture/react";
 
 const Slide = () => {
+  const containerRef = useRef(null);
+
   const [{ x, y, width, height }, api] = useSpring(() => ({
     x: 0,
     y: 0,
@@ -10,31 +12,39 @@ const Slide = () => {
     height: 200,
   }));
 
-  const bind = useDrag((state) => {
-    window.movement = state.movement;
-    window.offset = state.offset;
+  const bind = useDrag(
+    (state) => {
+      window.movement = state.movement;
+      window.offset = state.offset;
 
-    api.set(
-      {
+      api.set({
         x: state.offset[0],
         y: state.offset[1],
+      });
+    },
+    {
+      from: () => {
+        return [x.get(), y.get()];
       },
-      {
-        from: () => {
-          return [x.get(), y.get()];
-        },
-        bounds: () => {
-          return {
-            top: 0,
-            left: 0,
-          };
-        },
-      }
-    );
-  });
+      bounds: () => {
+        const containerWidth = containerRef.current?.clientWidth ?? 0;
+        const containerHeight = containerRef.current?.clientHeight ?? 0;
+
+        return {
+          top: 0,
+          left: 0,
+          right: containerWidth - width.get(),
+          bottom: containerHeight - height.get(),
+        };
+      },
+    }
+  );
 
   return (
-    <div className="flex justify-center items-center shadow-md rounded-lg min-h-[780px] m-6 bg-white">
+    <div
+      ref={containerRef}
+      className="flex shadow-md rounded-lg min-h-[780px] m-6 bg-white"
+    >
       <animated.div
         style={{ x, y, width, height }}
         className="border-dashed border-black border absolute touch-none cursor-nwse-resize z-10"
